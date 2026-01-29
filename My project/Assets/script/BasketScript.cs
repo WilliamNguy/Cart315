ae168a5f5ffe4c3cae6db9809c18a13f7dae00a6
WilliamNguy <144380539+WilliamNguy@users.noreply.github.com>
2026-01-29 14:05:27 -0500

    ?

---
+++ My project/Assets/script/BasketScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class BasketScript : MonoBehaviour
+
+    public KeyCode left;
+    public KeyCode right;
+
+    public float speed = 0.1f;
+
+    public float basket_x
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // void Start()
+    // {
+        
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        
+    }
+}
