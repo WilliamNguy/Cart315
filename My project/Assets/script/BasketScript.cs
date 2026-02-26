22002aadcc484b337837dafad6440b3da5a80a62
WilliamNguy <144380539+WilliamNguy@users.noreply.github.com>
2026-02-26 15:15:05 -0500

    ok

--- My project/Assets/script/BasketScript.cs
+++ My project/Assets/script/BasketScript.cs
@@ -1,24 +1,49 @@
 using UnityEngine;
 
-public class BasketScript : MonoBehaviour
+public class BasketScript : MonoBehaviour {
 
-    public KeyCode left;
-    public KeyCode right;
+    public float basket_x = 0;
+    public float speed = .1f;
 
-    public float speed = 0.1f;
+    public KeyCode left, right;
 
-    public float basket_x
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    // void Start()
-    // {
-        
-    // }
+    public float score;
+    
+    // Start is called before the first frame update
+    void Start() {
+        score = 0;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(left) && basket_x > -9f) {
+            basket_x -= speed;
+            
+        }
+        if (Input.GetKey(right) && basket_x < 9f) {
+            basket_x += speed;
+        }
+
+        this.transform.position = new Vector3(
+            basket_x, 
+            transform.position.y, 
+            transform.position.z
+        );
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log(other.gameObject.tag);
         
+        //Get the value of the building's points
+        int pointValue;
+        if (other.gameObject.tag == "Mall") score = 1;
+        // else if(other.gameObjects.tag == "Airport") score = 1;
+        else score = -1;
+        
+        UIManager.S.UpdateScore(pointValue);
+        
+        Destroy(other.gameObject);
+
     }
 }
